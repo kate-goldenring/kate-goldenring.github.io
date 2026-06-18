3c30ec056e653c42d0e174ad003be633a86b5729
Kate Goldenring <kate.goldenring@fermyon.com>
2026-06-17 22:37:41 -0700

    Make blog post navigatable by post ID
    Signed-off-by: Kate Goldenring <kate.goldenring@fermyon.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Gallery from './components/Gallery';
+import BlogPost from './components/BlogPost';
+import PostList from './components/admin/PostList';
+import PostForm from './components/admin/PostForm';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Gallery />} />
+        <Route path="/post/:id" element={<BlogPost />} />
+        <Route path="/admin" element={<PostList />} />
+        <Route path="/admin/new" element={<PostForm />} />
+        <Route path="/admin/edit/:id" element={<PostForm />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
